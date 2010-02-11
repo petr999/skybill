ad7ae19bf9335642fe943ebc37bf5d15ede1da4d
Peter Vereshagin <peter@vereshagin.org>
2010-02-11 19:39:35 +0400

    lic on sql

--- var/skybill.sql
+++ var/skybill.sql
@@ -1,6 +1,6 @@
 
 /*-
-* Copyright (c) [year] [your name]
+* Copyright (c) 2010 Peter Vereshagin
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
@@ -24,7 +24,6 @@
 * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 * SUCH DAMAGE.
 *
-* [id for your version control system, if any]
 */
 
 
